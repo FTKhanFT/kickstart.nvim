55b9a3098279381fa2b626f9f083bc1bd7c405aa
Tanvir Ahmed Khan <ftkhanft@gmail.com>
2024-05-30 19:06:32 +0600

    Modified flutter lsp

--- init.lua
+++ init.lua
@@ -255,6 +255,88 @@ require('lazy').setup({
       }
     end,
   },
+
+  {
+    'nvim-lua/plenary.nvim',
+    lazy = false,
+  },
+  {
+    'akinsho/flutter-tools.nvim',
+    lazy = false,
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'stevearc/dressing.nvim', -- optional for vim.ui.select
+    },
+    config = function()
+      require('flutter-tools').setup {
+        fvm = true,
+        ui = {
+          -- the border type to use for all floating windows, the same options/formats
+          -- used for ":h nvim_open_win" e.g. "single" | "shadow" | {<table-of-eight-chars>}
+          border = 'rounded',
+          -- This determines whether notifications are show with `vim.notify` or with the plugin's custom UI
+          -- please note that this option is eventually going to be deprecated and users will need to
+          -- depend on plugins like `nvim-notify` instead.
+          notification_style = 'native',
+        },
+        root_patterns = { '.git', 'pubspec.yaml' },
+        -- decorations = {
+        --   statusline = {
+        --     -- set to true to be able use the 'flutter_tools_decorations.app_version' in your statusline
+        --     -- this will show the current version of the flutter app from the pubspec.yaml file
+        --     app_version = true,
+        --     -- set to true to be able use the 'flutter_tools_decorations.device' in your statusline
+        --     -- this will show the currently running device if an application was started with a specific
+        --     -- device
+        --     device = true,
+        --     -- set to true to be able use the 'flutter_tools_decorations.project_config' in your statusline
+        --     -- this will show the currently selected project configuration
+        --     project_config = false,
+        --   },
+        -- },
+        flutter_lookup_cmd = 'dirname $(which flutter)',
+        widget_guides = {
+          enabled = true,
+        },
+        closing_tags = {
+          highlight = 'ErrorMsg', -- highlight for the closing tag
+          prefix = '>', -- character to use for close tag e.g. > Widget
+          enabled = true, -- set to false to disable
+        },
+        dev_log = {
+          enabled = true,
+          notify_errors = false, -- if there is an error whilst running then notify the user
+          open_cmd = 'tabedit', -- command to use to open the log buffer
+        },
+        dev_tools = {
+          autostart = false, -- autostart devtools server if not detected
+          auto_open_browser = false, -- Automatically opens devtools in the browser
+        },
+        outline = {
+          open_cmd = '30vnew', -- command to use to open the outline buffer
+          auto_open = false, -- if true this will open the outline automatically when it is first populated
+        },
+        lsp = {
+          color = { -- show the derived colours for dart variables
+            enabled = true, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
+            background = false, -- highlight the background
+            background_color = nil, -- required, when background is transparent (i.e. background_color = { r = 19, g = 17, b = 24},)
+            foreground = false, -- highlight the foreground
+            virtual_text = true, -- show the highlight using virtual text
+            virtual_text_str = '■', -- the virtual text character to highlight
+          },
+          settings = {
+            showTodos = true,
+            completeFunctionCalls = true,
+            analysisExcludedFolders = { 'dirname $(which flutter)', 'dirname $(which dart)', 'dirname $(which fvm)' },
+            renameFilesWithClasses = 'prompt', -- "always"
+            enableSnippets = true,
+            updateImportsOnRename = true, -- Whether to update imports and other directives when files are renamed. Required for `FlutterRename` command.
+          },
+        },
+      }
+    end,
+  },
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
 
@@ -643,87 +725,87 @@ require('lazy').setup({
     end,
   },
 
-  {
-    'nvim-lua/plenary.nvim',
-    lazy = false,
-  },
-  {
-    'akinsho/flutter-tools.nvim',
-    lazy = false,
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'stevearc/dressing.nvim', -- optional for vim.ui.select
-    },
-    config = function()
-      require('flutter-tools').setup {
-        fvm = true,
-        ui = {
-          -- the border type to use for all floating windows, the same options/formats
-          -- used for ":h nvim_open_win" e.g. "single" | "shadow" | {<table-of-eight-chars>}
-          border = 'rounded',
-          -- This determines whether notifications are show with `vim.notify` or with the plugin's custom UI
-          -- please note that this option is eventually going to be deprecated and users will need to
-          -- depend on plugins like `nvim-notify` instead.
-          notification_style = 'native',
-        },
-        root_patterns = { '.git', 'pubspec.yaml' },
-        -- decorations = {
-        --   statusline = {
-        --     -- set to true to be able use the 'flutter_tools_decorations.app_version' in your statusline
-        --     -- this will show the current version of the flutter app from the pubspec.yaml file
-        --     app_version = true,
-        --     -- set to true to be able use the 'flutter_tools_decorations.device' in your statusline
-        --     -- this will show the currently running device if an application was started with a specific
-        --     -- device
-        --     device = true,
-        --     -- set to true to be able use the 'flutter_tools_decorations.project_config' in your statusline
-        --     -- this will show the currently selected project configuration
-        --     project_config = false,
-        --   },
-        -- },
-        flutter_lookup_cmd = 'dirname $(which flutter)',
-        widget_guides = {
-          enabled = true,
-        },
-        closing_tags = {
-          highlight = 'ErrorMsg', -- highlight for the closing tag
-          prefix = '>', -- character to use for close tag e.g. > Widget
-          enabled = true, -- set to false to disable
-        },
-        dev_log = {
-          enabled = true,
-          notify_errors = false, -- if there is an error whilst running then notify the user
-          open_cmd = 'tabedit', -- command to use to open the log buffer
-        },
-        dev_tools = {
-          autostart = false, -- autostart devtools server if not detected
-          auto_open_browser = false, -- Automatically opens devtools in the browser
-        },
-        outline = {
-          open_cmd = '30vnew', -- command to use to open the outline buffer
-          auto_open = false, -- if true this will open the outline automatically when it is first populated
-        },
-        lsp = {
-          color = { -- show the derived colours for dart variables
-            enabled = true, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
-            background = false, -- highlight the background
-            background_color = nil, -- required, when background is transparent (i.e. background_color = { r = 19, g = 17, b = 24},)
-            foreground = false, -- highlight the foreground
-            virtual_text = true, -- show the highlight using virtual text
-            virtual_text_str = '■', -- the virtual text character to highlight
-          },
-          settings = {
-            showTodos = true,
-            completeFunctionCalls = true,
-            analysisExcludedFolders = { 'dirname $(which flutter)', 'dirname $(which dart)', 'dirname $(which fvm)' },
-            renameFilesWithClasses = 'prompt', -- "always"
-            enableSnippets = true,
-            updateImportsOnRename = true, -- Whether to update imports and other directives when files are renamed. Required for `FlutterRename` command.
-          },
-        },
-      }
-    end,
-  },
+  -- {
+  --   'nvim-lua/plenary.nvim',
+  --   lazy = false,
+  -- },
+  -- {
+  --   'akinsho/flutter-tools.nvim',
+  --   lazy = false,
+  --   dependencies = {
+  --     'nvim-lua/plenary.nvim',
+  --     'stevearc/dressing.nvim', -- optional for vim.ui.select
+  --   },
+  --   config = function()
+  --     require('flutter-tools').setup {
+  --       fvm = true,
+  --       ui = {
+  --         -- the border type to use for all floating windows, the same options/formats
+  --         -- used for ":h nvim_open_win" e.g. "single" | "shadow" | {<table-of-eight-chars>}
+  --         border = 'rounded',
+  --         -- This determines whether notifications are show with `vim.notify` or with the plugin's custom UI
+  --         -- please note that this option is eventually going to be deprecated and users will need to
+  --         -- depend on plugins like `nvim-notify` instead.
+  --         notification_style = 'native',
+  --       },
+  --       root_patterns = { '.git', 'pubspec.yaml' },
+  --       -- decorations = {
+  --       --   statusline = {
+  --       --     -- set to true to be able use the 'flutter_tools_decorations.app_version' in your statusline
+  --       --     -- this will show the current version of the flutter app from the pubspec.yaml file
+  --       --     app_version = true,
+  --       --     -- set to true to be able use the 'flutter_tools_decorations.device' in your statusline
+  --       --     -- this will show the currently running device if an application was started with a specific
+  --       --     -- device
+  --       --     device = true,
+  --       --     -- set to true to be able use the 'flutter_tools_decorations.project_config' in your statusline
+  --       --     -- this will show the currently selected project configuration
+  --       --     project_config = false,
+  --       --   },
+  --       -- },
+  --       flutter_lookup_cmd = 'dirname $(which flutter)',
+  --       widget_guides = {
+  --         enabled = true,
+  --       },
+  --       closing_tags = {
+  --         highlight = 'ErrorMsg', -- highlight for the closing tag
+  --         prefix = '>', -- character to use for close tag e.g. > Widget
+  --         enabled = true, -- set to false to disable
+  --       },
+  --       dev_log = {
+  --         enabled = true,
+  --         notify_errors = false, -- if there is an error whilst running then notify the user
+  --         open_cmd = 'tabedit', -- command to use to open the log buffer
+  --       },
+  --       dev_tools = {
+  --         autostart = false, -- autostart devtools server if not detected
+  --         auto_open_browser = false, -- Automatically opens devtools in the browser
+  --       },
+  --       outline = {
+  --         open_cmd = '30vnew', -- command to use to open the outline buffer
+  --         auto_open = false, -- if true this will open the outline automatically when it is first populated
+  --       },
+  --       lsp = {
+  --         color = { -- show the derived colours for dart variables
+  --           enabled = true, -- whether or not to highlight color variables at all, only supported on flutter >= 2.10
+  --           background = false, -- highlight the background
+  --           background_color = nil, -- required, when background is transparent (i.e. background_color = { r = 19, g = 17, b = 24},)
+  --           foreground = false, -- highlight the foreground
+  --           virtual_text = true, -- show the highlight using virtual text
+  --           virtual_text_str = '■', -- the virtual text character to highlight
+  --         },
+  --         settings = {
+  --           showTodos = true,
+  --           completeFunctionCalls = true,
+  --           analysisExcludedFolders = { 'dirname $(which flutter)', 'dirname $(which dart)', 'dirname $(which fvm)' },
+  --           renameFilesWithClasses = 'prompt', -- "always"
+  --           enableSnippets = true,
+  --           updateImportsOnRename = true, -- Whether to update imports and other directives when files are renamed. Required for `FlutterRename` command.
+  --         },
+  --       },
+  --     }
+  --   end,
+  -- },
   { -- Autoformat
     'stevearc/conform.nvim',
     lazy = false,
